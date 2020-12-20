bc3d290818a4e7ae3ab1b7bc0f74cbae89604891
ducluu27 <67710414+ducluu27@users.noreply.github.com>
2020-12-20 17:22:45 -0600

    update machine_learning.py with a basic linear gression model with input and output data

--- source/python/machine_learning.py
+++ source/python/machine_learning.py
@@ -1,2 +1,48 @@
-def isWorking():
-    return "yes"
+#def isWorking():
+ #   return "yes"
+# Resources
+import sqlite3
+import pandas as pd
+
+# Create a SQL connection to SQLite database
+con = sqlite3.connect("../../data/LIFE_EXPECTANCY_DB.db")
+cur = con.cursor()
+
+# The result of a "cursor.execute" can be iterated over by row
+for row in cur.execute('SELECT * FROM LIFE_EXPECTANCY_TABLE LIMIT 10;'):
+    print(row)
+
+# Read sqlite query results into a pandas DataFrame
+life_df = pd.read_sql_query("SELECT * from LIFE_EXPECTANCY_TABLE", con)
+
+# Be sure to close the SQLite db connection
+con.close()
+
+# Verify that result of SQL query is stored in the dataframe
+print(life_df.head(5))
+
+# Dataframe types
+print(life_df.dtypes)
+
+# Labeling input and output data
+y = life_df[life_df.columns[3]]
+X = life_df.Year.values.reshape(-1, 1)
+
+#import stuff
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+#LinearRegression
+model = LinearRegression()
+
+#Fit the model
+model.fit(X, y)
+
+#y_pred
+y_pred = model.predict(X)
+print(y_pred.shape)
+
+# Graph the life expectancy
+plt.scatter(X, y)
+plt.plot(X, y_pred, color='red')
+plt.show()
